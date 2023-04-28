d1f66f561c71eaf5da3110d5d3475f51802ed709
Sourabh Kamal <sourabh.k@sacumen.com>
2023-04-28 17:13:21 +0530

    updated the generator function

--- fico_generator.py
+++ fico_generator.py
@@ -2,11 +2,10 @@
 def fibo(first_value, second_value, limit):
     c=0
     while c<=limit:
-        result = first_value
-        yield result
+        yield first_value
         first_value, second_value = second_value, first_value + second_value
         c+=1
 
-FO = fibo(2,5,10)
+FO = fibo(0,1,10)
 for i in FO:
     print(i)
